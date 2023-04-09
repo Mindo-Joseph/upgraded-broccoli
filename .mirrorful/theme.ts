7a058d0604d0673f17b75ff9a732347280443ca0
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2023-04-09 13:37:57 +0300

    add colors from design

---
+++ .mirrorful/theme.ts
@@ -0,0 +1,50 @@
+export const Tokens = {
+  colors: {
+    primary: {
+      '500': '#03045E',
+      base: '#03045E',
+    },
+    secondary: {
+      '500': '#023E8A',
+      base: '#023E8A',
+    },
+    'blue-2': {
+      '500': '#0077B6',
+      base: '#0077B6',
+    },
+    'sky-blue': {
+      '500': '#0096C7',
+      base: '#0096C7',
+    },
+    'light-sky-blue': {
+      '500': '#00B4D8',
+      base: '#00B4D8',
+    },
+    'light-blue': {
+      '500': '#48CAE4',
+      base: '#48CAE4',
+    },
+    'lighter-blue': {
+      '500': '#90E0EF',
+      base: '#90E0EF',
+    },
+    'other-blue': {
+      '500': '#ADE8F4',
+      base: '#ADE8F4',
+    },
+    'lightest-blue': {
+      '500': '#CAF0F8',
+      base: '#CAF0F8',
+    },
+  },
+  fontSizes: {
+    sm: '1rem',
+    md: '1.2rem',
+    lg: '1.4rem',
+  },
+  boxShadows: {
+    sm: '1rem',
+    md: '1.2rem',
+    lg: '1.4rem',
+  },
+}
