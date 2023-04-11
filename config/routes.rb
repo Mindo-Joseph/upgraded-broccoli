f402b39007a60343da40c8f4325c3db02988f5d2
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2023-04-11 11:31:20 +0300

    add community section

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+
   get 'tutor/dashboard', to: 'tutor_dashboard#index', as: 'tutor_dashboard'
 
     authenticate :user, lambda { |u| u.admin? } do
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   root to: 'home#index'
   get 'learner/dashboard', to: 'learner_dashboard#index', as: 'learner_dashboard'
+  get 'community', to: 'community#index', as: :community
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
